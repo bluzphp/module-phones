0f5248ea770153fe25c0c7e4881a86ece81c564d
Anton Shevchuk <antonshevchuk@gmail.com>
2021-12-30 10:35:00 +0200

    Updated migration declaration

--- data/migrations/20171116162227_users_phones_permissions.php
+++ data/migrations/20171116162227_users_phones_permissions.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 use Phinx\Migration\AbstractMigration;
 
-class UsersPhonesPermissions extends AbstractMigration
+final class UsersPhonesPermissions extends AbstractMigration
 {
     /**
      * Migrate Up.
